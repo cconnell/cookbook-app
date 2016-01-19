2e5509cc06b4be18db8d2991007ce701d7a7f736
Chris Connell <chris.g.connell@gmail.com>
2016-01-19 11:43:10 -0600

    new repo for cookbook-app

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,56 @@
+class RecipesController < ApplicationController
+
+  def meal
+    @recipe = Recipe.last
+  end
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+  end
+
+ def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def create
+    @recipe = Recipe.create( {title: params[:title],
+                              chef: params[:chef], 
+                              ingredients: params[:ingredients], 
+                              directions: params[:directions]
+                              })
+    flash[:success] = "New Recipe Created"
+    
+    redirect_to "/"
+end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find_by(id: params[:id])
+
+    @recipe.update({
+                              title: params[:title],
+                              chef: params[:chef],
+                              ingredients: params[:ingredients],
+                              directions: params[:directions]
+
+      })
+    flash[:success] = "Recipe Updated"
+    redirect_to "/recipes/#{@recipe[:id]}"
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+
+    flash[:success] = "You did such a great job destroying that recipe"
+    flash[:warning] = "Recipe Destroyed"
+
+  redirect_to "/"
+  end
+end
